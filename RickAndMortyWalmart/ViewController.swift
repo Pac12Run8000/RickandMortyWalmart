97856b0b4cbaf76ff46f9cd9717f48081a300254
Pac12Run8000 <gangstarr78992@>
2022-05-21 16:09:36 -0700

    Initial Commit

---
+++ RickAndMortyWalmart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyWalmart
+//
+//  Created by Michelle Grover on 5/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
